58fc502de955cdd32e7a6b0a17d38564396d6eaf
ASHISH_ANAND <116444962+AshishAnand1511@users.noreply.github.com>
2024-09-30 15:22:39 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -11,7 +11,9 @@ var isValid = function(s) {
         ']': "["
     };
     for (let char of s) {
-        if(char === '(' || char ==="{" || char === "[")
+        if (char === '(' || char === "{" || char === "[") {
+            stack.push(char);
+        }
     }
 };
 
